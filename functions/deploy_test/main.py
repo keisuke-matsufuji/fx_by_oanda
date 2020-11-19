0785b02a2044a6245c75dc3bc61fc5762f20c2dd
Keisuke Matsufuji <mt.fuji06781@gmail.com>
2020-11-19 21:12:58 +0900

    deploy test

--- functions/deploy_test/main.py
+++ functions/deploy_test/main.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
 
     jst = pytz.timezone('Asia/Tokyo')
     jst_now = datetime.datetime.now(tz=jst)
-    print('jst_now!!!')
+    print('jst_now!!!!')
     print(jst_now)
 
     print('all done')
