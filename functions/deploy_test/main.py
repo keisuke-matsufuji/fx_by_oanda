03cf1faccf185e357def3a78626f7778dd3306ba
Keisuke Matsufuji <mt.fuji06781@gmail.com>
2020-11-21 00:06:36 +0900

    dynamo_db test

--- functions/deploy_test/main.py
+++ functions/deploy_test/main.py
@@ -5,6 +5,11 @@ def lambda_handler(event, context):
 
     print('run')
 
+    message = settings.ENV_PARAM_TEST
+
+    print('message')
+    print(message)
+
     dynamodb = boto3.client('dynamodb', endpoint_url=settings.DYNAMODB_ENDPOINT)
 
     print('dynamodb')
--- functions/deploy_test/settings.py
+++ functions/deploy_test/settings.py
@@ -8,5 +8,7 @@ load_dotenv(dotenv_path)
 # 環境変数の値を代入
 DYNAMODB_ENDPOINT = os.environ.get("DYNAMODB_ENDPOINT", None)
 
+ENV_PARAM_TEST = os.environ.get("ENV_PARAM_TEST", None)
+
 OANDA_ACCOUNT_ID = os.environ.get("OANDA_API_ACCOUNT_ID") 
 OANDA_ACCESS_TOKEN = os.environ.get("OANDA_API_ACCESS_TOKEN") 
