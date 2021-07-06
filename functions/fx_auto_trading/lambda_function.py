55a7560a9b74ee9fb75d2420ded3ccddde73dd62
Keisuke Matsufuji <mt.fuji06781@gmail.com>
2021-07-07 00:05:08 +0900

    デプロイテスト

--- functions/fx_auto_trading/lambda_function.py
+++ functions/fx_auto_trading/lambda_function.py
@@ -23,8 +23,8 @@ import pandas as pd
 import pandas.tseries.offsets as offsets
 # 自作モジュールのインポート
 # from ディレクトリ名 import モジュール名
-# from db import price_log
-# from api import oanda_api
+from db import price_log
+from api import oanda_api
 # from api import OandaApi
 # from . import db, api
 # import price_log
